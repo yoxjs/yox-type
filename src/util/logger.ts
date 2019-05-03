3b729ed22362340cc2b344c452506fdf25dc7929
musicode <musicode@qq.com>
2019-05-03 16:40:40 +0800

    remove util

--- src/util/logger.ts
+++
@@ -1,7 +0,0 @@
-export interface log { (msg: string): void }
-
-export interface warn { (msg: string): void }
-
-export interface error { (msg: string): void }
-
-export interface fatal { (msg: string): never }
