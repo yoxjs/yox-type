123b02cc83ee099df6d1637733a77198b7cba832
musicode <musicode@qq.com>
2019-06-20 11:51:53 +0800

    update

--- src/type.ts
+++ src/type.ts
@@ -1,56 +1,64 @@
-import * as config from '../../yox-config/src/config'
+import {
+  Yox,
+  YoxOptions,
+  ComputedOptions,
+  WatcherOptions,
+  DirectiveHooks,
+  TransitionHooks,
+  SpecialEventHooks,
+} from './global'
 
-export declare type hint = 1 | 2 | 3
+export type hint = 1 | 2 | 3
 
-export declare type lazy = number | true
+export type lazy = number | true
 
-export declare type propType = (key: string, value: any) => void
+export type propType = (key: string, value: any) => void
 
-export declare type propValue = () => any
+export type propValue = () => any
 
-export declare type data = Record<string, any>
+export type data = Record<string, any>
 
-export declare type dataGenerator = (options: YoxOptions) => data
+export type dataGenerator = (options: YoxOptions) => data
 
-export declare type getter = () => any
+export type getter = () => any
 
-export declare type setter = (value: any) => void
+export type setter = (value: any) => void
 
-export declare type formater = (...args: any) => string | number | boolean
+export type formater = (...args: any) => string | number | boolean
 
-export declare type filter = formater | Record<string, formater>
+export type filter = formater | Record<string, formater>
 
-export declare type watcher = (newValue: any, oldValue: any, keypath: string) => void
+export type watcher = (newValue: any, oldValue: any, keypath: string) => void
 
-export declare type listener = (event: CustomEvent, data?: data) => false | void
+export type listener = (event: CustomEvent, data?: data) => false | void
 
-export declare type nativeListener = (event: CustomEvent | Event) => false | void
+export type nativeListener = (event: CustomEvent | Event) => false | void
 
-export declare type enter = (node: HTMLElement) => void
+export type enter = (node: HTMLElement) => void
 
-export declare type leave = (node: HTMLElement, done: () => void) => void
+export type leave = (node: HTMLElement, done: () => void) => void
 
-export declare type bind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
+export type bind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
 
-export declare type unbind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
+export type unbind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
 
-export declare type on = (node: HTMLElement | Window | Document, listener: nativeListener) => void
+export type on = (node: HTMLElement | Window | Document, listener: nativeListener) => void
 
-export declare type off = (node: HTMLElement | Window | Document, listener: nativeListener) => void
+export type off = (node: HTMLElement | Window | Document, listener: nativeListener) => void
 
-export declare type componentCallback = (options: YoxOptions) => void
+export type componentCallback = (options: YoxOptions) => void
 
-export declare type componentLoader = (callback: componentCallback) => void
+export type componentLoader = (callback: componentCallback) => void
 
-export declare type component = YoxOptions | componentLoader
+export type component = YoxOptions | componentLoader
 
-export declare type optionsBeforeCreateHook = (options: YoxOptions) => void
+export type optionsBeforeCreateHook = (options: YoxOptions) => void
 
-export declare type optionsOtherHook = () => void
+export type optionsOtherHook = () => void
 
-export declare type routerBeforeHook = (to: Location, from: Location | void, next: (value?: false | string | RouteTarget) => void) => void
+export type routerBeforeHook = (to: Location, from: Location | void, next: (value?: false | string | RouteTarget) => void) => void
 
-export declare type routerAfterHook = (to: Location, from: Location | void) => void
+export type routerAfterHook = (to: Location, from: Location | void) => void
 
 export interface ValueHolder {
   keypath?: string
@@ -224,140 +232,6 @@ export interface API {
 
 }
 
-interface arrayUtil {
-
-  each<T>(
-    array: T[],
-    callback: (item: T, index: number, length: number) => boolean | void,
-    reversed?: boolean
-  ): void
-
-  push<T>(array: T[], target: T | T[]): void
-
-  unshift<T>(array: T[], target: T | T[]): void
-
-  indexOf<T>(array: T[], target: T, strict?: boolean): number
-
-  last<T>(array: T[]): T | void
-
-  pop<T>(array: T[]): T | void
-
-  remove<T>(array: T[], target: T, strict?: boolean): number
-
-  has<T>(array: T[], target: T, strict?: boolean): boolean
-
-  toArray<T>(array: T[] | ArrayLike<T>): T[]
-
-  toObject(array: any[], key?: string | null, value?: any): Object
-
-  join(array: string[], separator: string): string
-
-  falsy(array: any): boolean
-
-}
-
-interface isUtil {
-
-  func(value: any): boolean
-
-  array(value: any): boolean
-
-  object(value: any): boolean
-
-  string(value: any): boolean
-
-  number(value: any): boolean
-
-  boolean(value: any): boolean
-
-  numeric(value: any): boolean
-
-}
-
-interface loggerUtil {
-
-  DEBUG: number
-
-  INFO: number
-
-  WARN: number
-
-  ERROR: number
-
-  FATAL: number
-
-  debug(msg: string, tag?: string): void
-
-  info(msg: string, tag?: string): void
-
-  warn(msg: string, tag?: string): void
-
-  error(msg: string, tag?: string): void
-
-  fatal(msg: string, tag?: string): void
-
-}
-
-interface objectUtil {
-
-  keys(object: data): string[]
-
-  sort(object: data, desc?: boolean): string[]
-
-  each(object: data, callback: (value: any, key: string) => boolean | void): void
-
-  clear(object: data): void
-
-  extend(original: data, object: data): data
-
-  merge(object1: data | void, object2: data | void): data | void
-
-  copy(object: any, deep?: boolean): any
-
-  get(object: any, keypath: string): ValueHolder | undefined
-
-  set(object: data, keypath: string, value: any, autofill?: boolean): void
-
-  has(object: data, key: string | number): boolean
-
-  falsy(object: any): boolean
-
-}
-
-interface stringUtil {
-
-  camelize(str: string): string
-
-  hyphenate(str: string): string
-
-  capitalize(str: string): string
-
-  trim(str: any): string
-
-  slice(str: string, start: number, end?: number): string
-
-  indexOf(str: string, part: string, start?: number): number
-
-  lastIndexOf(str: string, part: string, end?: number): number
-
-  startsWith(str: string, part: string): boolean
-
-  endsWith(str: string, part: string): boolean
-
-  charAt(str: string, index?: number): string
-
-  codeAt(str: string, index?: number): number
-
-  upper(str: string): string
-
-  lower(str: string): string
-
-  has(str: string, part: string): boolean
-
-  falsy(str: any): boolean
-
-}
-
 export interface Task {
 
   // 待执行的函数
@@ -380,41 +254,6 @@ export interface NextTask {
 
 }
 
-export interface ComputedOptions {
-
-  // getter，必填
-  get: getter
-
-  // setter
-  set?: setter
-
-  // 是否开启缓存，默认为 true
-  cache?: boolean
-
-  // 是否同步监听变化，默认为 true
-  sync?: boolean
-
-  // 写死依赖，从而跳过依赖自动收集
-  deps?: string[]
-
-}
-
-export interface WatcherOptions {
-
-  // 数据变化处理器，必填
-  watcher: watcher
-
-  // 是否立即执行一次 watcher，默认为 false
-  immediate?: boolean
-
-  // 是否同步监听变化，默认为 false
-  sync?: boolean
-
-  // 是否只监听一次，默认为 false
-  once?: boolean
-
-}
-
 export interface EmitterOptions extends Task {
 
   // 所在的命名空间
@@ -428,82 +267,6 @@ export interface EmitterOptions extends Task {
 
   // 计数器，用于扩展，随便做什么计数都行
   count?: number
-
-}
-
-export interface YoxOptions {
-
-  propTypes?: Record<string, PropRule>
-
-  el?: string | Node
-
-  data?: data | dataGenerator
-
-  template?: string | Function
-
-  model?: string
-
-  props?: data
-
-  root?: Yox
-
-  parent?: Yox
-
-  context?: Yox
-
-  replace?: true
-
-  vnode?: VNode
-
-  slots?: Record<string, VNode[]>
-
-  computed?: Record<string, getter | ComputedOptions>
-
-  watchers?: Record<string, watcher | WatcherOptions>
-
-  transitions?: Record<string, TransitionHooks>
-
-  components?: Record<string, YoxOptions>
-
-  directives?: Record<string, DirectiveHooks>
-
-  partials?: Record<string, string>
-
-  filters?: Record<string, filter>
-
-  events?: Record<string, listener>
-
-  methods?: Record<string, Function>
-
-  extensions?: data
-
-  [config.HOOK_BEFORE_CREATE]?: optionsBeforeCreateHook
-
-  [config.HOOK_AFTER_CREATE]?: optionsOtherHook
-
-  [config.HOOK_BEFORE_MOUNT]?: optionsOtherHook
-
-  [config.HOOK_AFTER_MOUNT]?: optionsOtherHook
-
-  [config.HOOK_BEFORE_UPDATE]?: optionsOtherHook
-
-  [config.HOOK_AFTER_UPDATE]?: optionsOtherHook
-
-  [config.HOOK_BEFORE_DESTROY]?: optionsOtherHook
-
-  [config.HOOK_AFTER_DESTROY]?: optionsOtherHook
-
-  [config.HOOK_BEFORE_ROUTE_ENTER]?: routerBeforeHook
-
-  [config.HOOK_AFTER_ROUTE_ENTER]?: routerAfterHook
-
-  [config.HOOK_BEFORE_ROUTE_UPDATE]?: routerBeforeHook
-
-  [config.HOOK_AFTER_ROUTE_UPDATE]?: routerAfterHook
-
-  [config.HOOK_BEFORE_ROUTE_LEAVE]?: routerBeforeHook
-
-  [config.HOOK_AFTER_ROUTE_LEAVE]?: routerAfterHook
 
 }
 
@@ -701,229 +464,10 @@ export declare var Computed: {
 
 }
 
-export interface Yox {
-
-  $options: YoxOptions
-
-  $emitter: Emitter
-
-  $observer: Observer
-
-  $el?: HTMLElement
-
-  $vnode?: VNode
-
-  $model?: string
-
-  $root?: Yox
-
-  $parent?: Yox
-
-  $context?: Yox
-
-  $children?: Yox[]
-
-  $refs?: Record<string, Yox | HTMLElement>
-
-  addComputed(
-    keypath: string,
-    computed: getter | ComputedOptions
-  ): Computed | void
-
-  removeComputed(
-    keypath: string
-  ): void
-
-  get(
-    keypath: string,
-    defaultValue?: any,
-    depIgnore?: boolean
-  ): any
-
-  set(
-    keypath: string | data,
-    value?: any
-  ): void
-
-  on(
-    type: string | Record<string, listener>,
-    listener?: listener
-  ): Yox
-
-  once(
-    type: string | Record<string, listener>,
-    listener?: listener
-  ): Yox
-
-  off(
-    type?: string,
-    listener?: listener
-  ): Yox
-
-  fire(
-    type: string | CustomEvent,
-    data?: data | boolean,
-    downward?: boolean
-  ): boolean
-
-  watch(
-    keypath: string | Record<string, watcher | WatcherOptions>,
-    watcher?: watcher | WatcherOptions,
-    immediate?: boolean
-  ): Yox
-
-  unwatch(
-    keypath?: string,
-    watcher?: watcher
-  ): Yox
-
-  loadComponent(
-    name: string,
-    callback: componentCallback
-  ): void
-
-  createComponent(
-    options: YoxOptions,
-    vnode: VNode
-  ): Yox
-
-  directive(
-    name: string | Record<string, DirectiveHooks>,
-    directive?: DirectiveHooks
-  ): DirectiveHooks | void
-
-  transition(
-    name: string | Record<string, TransitionHooks>,
-    transition?: TransitionHooks
-  ): TransitionHooks | void
-
-  component(
-    name: string | Record<string, component>,
-    component?: component
-  ): component | void
-
-  partial(
-    name: string | Record<string, string>,
-    partial?: string
-  ): Function | void
-
-  filter(
-    name: string | Record<string, filter>,
-    filter?: filter
-  ): filter | void
-
-  checkProps(props: data): void
-
-  checkProp(key: string, value: any): void
-
-  forceUpdate(data?: data): void
-
-  destroy(): void
-
-  nextTick(task: Function): void
-
-  toggle(keypath: string): boolean
-
-  increase(keypath: string, step?: number, max?: number): number | void
-
-  decrease(keypath: string, step: number, min?: number): number | void
-
-  insert(keypath: string, item: any, index: number | boolean): true | void
-
-  append(keypath: string, item: any): true | void
-
-  prepend(keypath: string, item: any): true | void
-
-  removeAt(keypath: string, index: number): true | void
-
-  remove(keypath: string, item: any): true | void
-
-  copy<T>(data: T, deep?: boolean): T
-
-}
-
-export declare var Yox: {
-
-  prototype: Yox
-
-  dom: API
-
-  is: isUtil
-
-  array: arrayUtil
-
-  object: objectUtil
-
-  string: stringUtil
-
-  logger: loggerUtil
-
-  Emitter: typeof Emitter
-
-  Event: typeof CustomEvent
-
-  new(options?: YoxOptions): Yox
-
-  use(plugin: YoxPlugin): void
-
-  nextTick(task: Function, context?: any): void
-
-  compile(template: string, stringify?: boolean): Function | string
-
-  directive(
-    name: string | Record<string, DirectiveHooks>,
-    directive?: DirectiveHooks
-  ): DirectiveHooks | void
-
-  transition(
-    name: string | Record<string, TransitionHooks>,
-    transition?: TransitionHooks
-  ): TransitionHooks | void
-
-  component(
-    name: string | Record<string, component>,
-    component?: component
-  ): component | void
-
-  partial(
-    name: string | Record<string, string>,
-    partial?: string
-  ): Function | void
-
-  filter(
-    name: string | Record<string, filter>,
-    filter?: filter
-  ): filter | void
-
-}
-
-declare type YoxClass = typeof Yox
-
-export interface YoxPlugin {
-  version: string
-  install(Yox: YoxClass): void
-}
-
 export interface PropRule {
   type: string | string[] | propType
   value?: any | propValue
   required?: boolean
-}
-
-export interface DirectiveHooks {
-  once?: true
-  bind: bind
-  unbind?: unbind
-}
-
-export interface SpecialEventHooks {
-  on: on
-  off: off
-}
-
-export interface TransitionHooks {
-  enter?: enter
-  leave?: leave
 }
 
 export interface Location {
