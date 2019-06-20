cdda9fc732fe3ccd60da0e17501334d282620f3c
musicode <musicode@qq.com>
2019-06-20 11:04:25 +0800

    update

--- src/type.ts
+++ src/type.ts
@@ -1,56 +1,56 @@
 import * as config from '../../yox-config/src/config'
 
-export type hint = 1 | 2 | 3
+export declare type hint = 1 | 2 | 3
 
-export type lazy = number | true
+export declare type lazy = number | true
 
-export type propType = (key: string, value: any) => void
+export declare type propType = (key: string, value: any) => void
 
-export type propValue = () => any
+export declare type propValue = () => any
 
-export type data = Record<string, any>
+export declare type data = Record<string, any>
 
-export type dataGenerator = (options: YoxOptions) => data
+export declare type dataGenerator = (options: YoxOptions) => data
 
-export type getter = () => any
+export declare type getter = () => any
 
-export type setter = (value: any) => void
+export declare type setter = (value: any) => void
 
-export type formater = (...args: any) => string | number | boolean
+export declare type formater = (...args: any) => string | number | boolean
 
-export type filter = formater | Record<string, formater>
+export declare type filter = formater | Record<string, formater>
 
-export type watcher = (newValue: any, oldValue: any, keypath: string) => void
+export declare type watcher = (newValue: any, oldValue: any, keypath: string) => void
 
-export type listener = (event: CustomEvent, data?: data) => false | void
+export declare type listener = (event: CustomEvent, data?: data) => false | void
 
-export type nativeListener = (event: CustomEvent | Event) => false | void
+export declare type nativeListener = (event: CustomEvent | Event) => false | void
 
-export type enter = (node: HTMLElement) => void
+export declare type enter = (node: HTMLElement) => void
 
-export type leave = (node: HTMLElement, done: () => void) => void
+export declare type leave = (node: HTMLElement, done: () => void) => void
 
-export type bind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
+export declare type bind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
 
-export type unbind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
+export declare type unbind = (node: HTMLElement | Yox, directive: Directive, vnode: VNode) => void
 
-export type on = (node: HTMLElement | Window | Document, listener: nativeListener) => void
+export declare type on = (node: HTMLElement | Window | Document, listener: nativeListener) => void
 
-export type off = (node: HTMLElement | Window | Document, listener: nativeListener) => void
+export declare type off = (node: HTMLElement | Window | Document, listener: nativeListener) => void
 
-export type componentCallback = (options: YoxOptions) => void
+export declare type componentCallback = (options: YoxOptions) => void
 
-export type componentLoader = (callback: componentCallback) => void
+export declare type componentLoader = (callback: componentCallback) => void
 
-export type component = YoxOptions | componentLoader
+export declare type component = YoxOptions | componentLoader
 
-export type optionsBeforeCreateHook = (options: YoxOptions) => void
+export declare type optionsBeforeCreateHook = (options: YoxOptions) => void
 
-export type optionsOtherHook = () => void
+export declare type optionsOtherHook = () => void
 
-export type routerBeforeHook = (to: Location, from: Location | void, next: (value?: false | string | RouteTarget) => void) => void
+export declare type routerBeforeHook = (to: Location, from: Location | void, next: (value?: false | string | RouteTarget) => void) => void
 
-export type routerAfterHook = (to: Location, from: Location | void) => void
+export declare type routerAfterHook = (to: Location, from: Location | void) => void
 
 export interface ValueHolder {
   keypath?: string
@@ -897,7 +897,7 @@ export declare var Yox: {
 
 }
 
-type YoxClass = typeof Yox
+declare type YoxClass = typeof Yox
 
 export interface YoxPlugin {
   version: string
