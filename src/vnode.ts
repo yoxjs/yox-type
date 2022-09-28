f0e818fc276ccf01dcbda44e8bf4a66b2b3cf5ac
musicode <musicode@qq.com>
2022-09-28 08:54:31 +0800

    vnode 删除 isComment、isFragment、isPortal、isSlot 属性

--- src/vnode.ts
+++ src/vnode.ts
@@ -134,18 +134,6 @@ export interface VNode {
   // 是否是 组件节点
   readonly isComponent?: boolean
 
-  // 是否是 注释节点
-  readonly isComment?: boolean
-
-  // 是否是 片段节点
-  readonly isFragment?: boolean
-
-  // 是否是 传送门节点
-  readonly isPortal?: boolean
-
-  // 是否是 插槽节点
-  readonly isSlot?: boolean
-
   // 是否是 svg 元素
   readonly isSvg?: boolean
 
